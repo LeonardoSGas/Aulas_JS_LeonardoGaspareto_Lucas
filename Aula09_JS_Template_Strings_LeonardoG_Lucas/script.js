2b936cf04e865e7153e322e7a1fb2449f0cedbe3
LeonardoSGas <leonardo.gaspareto@escola.pr.gov.br>
2024-10-28 08:22:43 -0300

    Add files via upload

---
+++ Aula09_JS_Template_Strings_LeonardoG_Lucas/script.js
@@ -0,0 +1,9 @@
+let nome = "Leonardo";
+let sobreNome = "Gaspareto";
+let idade = 16;
+
+let mensagem = "Meu nome é "+nome+" "+sobreNome+" e minha idade é "+idade+" anos.";
+console.log(mensagem);
+
+let mensagem2=`Meu nome é ${nome} ${sobreNome} e minha idade é ${idade} anos`;
+console.log(mensagem2);
