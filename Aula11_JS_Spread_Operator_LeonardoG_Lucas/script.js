f35563cdea661af00c9a746ba1df98bf8feac2c5
LeonardoSGas <leonardo.gaspareto@escola.pr.gov.br>
2024-10-30 08:53:16 -0300

    Add files via upload

---
+++ Aula11_JS_Spread_Operator_LeonardoG_Lucas/script.js
@@ -0,0 +1,41 @@
+//spread operator
+let primeiros = [1,2,3];
+
+let numeros = [primeiros,4,5,6];
+
+let numeros2 = [...primeiros,4,5,6];
+
+console.log(numeros);
+console.log(numeros2);
+
+//spredando num objeto
+let pessoa = {
+    nome:'Leonardo',
+    idade:16,
+    cargo:'Aluno'
+
+};
+
+let dadosPessoa = {
+    ...pessoa,
+    status:'ativo',
+    cidade:'Campo Largo'
+
+};
+
+console.log(dadosPessoa);
+console.log(dadosPessoa.cargo);
+
+//exemplificando de uma forma mais realista 
+
+function novoUsuario(info){
+    //console.log(info);
+    let dados = {
+        ...info,
+        status:'ativo',
+        inicio:'20/09/2024',
+        codigo:123
+    };
+    console.log(dados)
+};
+novoUsuario({nome:'Admilson', sobrenome:'Rico', cargo:'estágiario'});
