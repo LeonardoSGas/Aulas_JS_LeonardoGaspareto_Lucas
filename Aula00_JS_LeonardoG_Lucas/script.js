c321153ffb78dd54b7a896d503441049849f65e3
LeonardoSGas <leonardo.gaspareto@escola.pr.gov.br>
2024-11-13 08:04:16 -0300

    Add files via upload

---
+++ Aula00_JS_LeonardoG_Lucas/script.js
@@ -0,0 +1,14 @@
+
+//alert("testando o screipt");
+
+var nome = prompt ("Qual seu nome? ");
+
+var sobrenome = prompt ("Qual seu sobrenome? ");
+
+console.log ("Olá "+nome+" "+sobrenome );
+
+//saida do DOM
+document.write("<h1>Bem vindo "+nome+" "+sobrenome+"</h1>");
+document.write("Steve atravez do java script")
+
+document.write('<img src="https://sujeitoprogramador.com/steve.png" alt=foto do steve />')
